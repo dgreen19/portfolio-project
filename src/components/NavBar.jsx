d86e1afe9ba1a9a6b53932f404f26bc75897fbc4
Darryl <green.jr.darryl@gmail.com>
2022-09-13 11:05:44 -0400

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+      <div>
+          <li>
+              <ul>
+                About
+              </ul>
+              <ul>
+                  Experience
+              </ul>
+              <ul>
+                  Education
+              </ul>
+              <ul>
+                  Projects
+              </ul>
+              <ul>
+                  Skills
+              </ul>
+              <ul>
+                  Contact
+              </ul>
+          </li>
+      </div>
+  )
+}
