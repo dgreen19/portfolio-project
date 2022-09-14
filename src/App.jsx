45e5dd741d93b91e6ac15b5305ffc0b0fb08f131
Darryl <green.jr.darryl@gmail.com>
2022-09-14 18:14:43 -0400

    Added basic components and setup basic layout. Removed bootstrap and Sass packages, added Bootstrap CDN.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import Header from './components/Header'
 import NavBar from './components/NavBar'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+import WorkHistory from './components/WorkHistory'
+import LightDark from './components/LightDark'
+import Education from './components/Education'
+import Contact from './components/Contact'
 
 
 export default function App() {
   return (
-    <div>
-      <NavBar></NavBar>
-    </div>
-  )
+		<div>
+			<NavBar>
+				<div>
+					<LightDark></LightDark>
+				</div>
+			</NavBar>
+			<Header></Header>
+			<Skills></Skills>
+			<WorkHistory></WorkHistory>
+			<Projects></Projects>
+			<Education></Education>
+			<Contact></Contact>
+		</div>
+	);
 }
