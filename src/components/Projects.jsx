962496b1384a6b601cc2ed8069ae5cdf9a428d4f
Darryl <green.jr.darryl@gmail.com>
2023-12-17 17:25:40 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+
+import Project1 from '../assets/project1.png'
+import Project2 from '../assets/project2.png'
+import Project3 from '../assets/project3.png'
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Project1}
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Project2}
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Project3}
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
   )
 }
+
+
