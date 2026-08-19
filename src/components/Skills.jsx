0e1dca1c7622acfcc633643e222882cf6a4a9017
Darryl <green.jr.darryl@gmail.com>
2026-08-18 21:48:07 -0400

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Skills() {
   return (
-    <div>Skills</div>
+    <div>Skills
+      <ul>
+        <li>TypeScript</li>
+        <li>JavaScript</li>
+        <li>HTML/CSS</li>
+        <li>React</li>
+        <li>Node.js</li>
+      </ul>
+    </div>
   )
 }
