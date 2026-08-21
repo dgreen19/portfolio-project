8be5a72d0addd1eb26a3c0a106cdfe7e46f04b8c
Darryl <green.jr.darryl@gmail.com>
2026-08-21 12:33:07 -0400

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 export default function AboutMe() {
   return (
-    <div>AboutMe</div>
-    <div>
-        <Hello></Hello>
-    </div>
+    <><div>AboutMe</div><div>
+      <Hello></Hello>
+    </div></>
   )
 }
